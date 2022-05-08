1d09bbbb193dc6ac7706842f1cd840d29779e1ef
Altif Ali <altif.ali@paceit.co.uk>
2022-05-08 21:21:16 +0100

    Multiplication and division features done plus some refactoring

--- js/test_money.js
+++ js/test_money.js
@@ -1,3 +1,4 @@
+const { deepStrictEqual } = require('assert');
 const assert = require('assert');
 
 class Money {
@@ -10,16 +11,23 @@ class Money {
 
     }
 
+    divide(divisor) {
+        return new Money(this.amount / divisor, this.currency)
+
+    }
+
+
 }
 
-
-let fiver = new Money(5, "USD");
-let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
-assert.strictEqual(tenner.currency, "USD");
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+deepStrictEqual(fiveDollars.times(2), tenDollars);
 
 let tenEuros = new Money(10,"EUR");
-let twentyEuros = tenEuros.times(2);
+let twentyEuros = new Money(20, "EUR");
+deepStrictEqual(tenEuros.times(2), twentyEuros);
 
-assert.strictEqual(twentyEuros.amount, 20);
-assert.strictEqual(twentyEuros.currency, "EUR");
+let originalMoney = new Money(4002, "KRW");
+let acctualMoneyAfterDivision = originalMoney.divide(4);
+let expectMoneyAfterDivision = new Money(1000.5, "KRW");
+assert.deepStrictEqual(acctualMoneyAfterDivision, expectMoneyAfterDivision) 
